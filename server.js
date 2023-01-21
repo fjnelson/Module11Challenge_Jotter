4aa4465a576af92171e655620adf72125a075705
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-01-21 11:55:24 -0800

    using routes

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const htmlRoutes = require('./routes/htmlRoutes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
 app.listen(PORT, () => {
     console.log(`Opened on ${PORT}`);
 });
