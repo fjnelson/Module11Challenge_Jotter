d7264c455bedac264fbcfabab348561b47888e9f
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-01-21 11:58:26 -0800

    adding data save

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,22 +1,22 @@
 const router = require('express').Router();
-const saveData = require('../db/saveData');
+const dataSave = require('../db/data');
 
 router.get('/notes', function (req, res) {
-    saveData
+    dataSave
         .retrieveNotes()
         .then(notes => res.json(notes))
         .catch(err => res.status(500).json(err));
 });
 
 router.post('/notes', (req, res) => {
-    saveData
+    dataSave
         .addNote(req.body)
         .then((note) => res.json(note))
         .catch(err => res.status(500).json(err));
 });
 
 router.delete('/notes/:id', function (req, res) {
-    saveData
+    dataSave
         .deleteNote(req.params.id)
         .then(() => res.json({ ok: true }))
         .catch(err => res.status(500).json(err));
