8d7af8240e53690b759ac92622cd8aa3a58daf34
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-01-21 11:52:49 -0800

    adding routes

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
-
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 const app = express();
-
 const PORT = process.env.PORT || 3001;
-
-
 
 app.listen(PORT, () => {
     console.log(`Opened on ${PORT}`);
