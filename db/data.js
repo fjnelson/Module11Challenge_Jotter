3263f8c6bbddb6c2e5ba90249b1c2ea4dad4893c
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-01-22 15:10:05 -0800

    adding data

--- db/data.js
+++ db/data.js
@@ -2,26 +2,26 @@ const util = require('util');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
-const readNote = util.promisify(fs.readFile);
-const writeNote = util.promisify(fs.writeFile);
+const readData = util.promisify(fs.readFile);
+const writeData = util.promisify(fs.writeFile);
 class Save {
     write(note) {
-        return writeNote('db/db.json', JSON.stringify(note));
+        return writeData('db/db.json', JSON.stringify(note));
     }
 
     read() {
-        return readNote('db/db.json', 'utf8');
+        return readData('db/db.json', 'utf8');
     }
 
     retrieveNotes() {
         return this.read().then(notes => {
-            let parsedNotes;
+            let dataNotes;
             try {
-                parsedNotes = [].concat(JSON.parse(notes));
+                dataNotes = [].concat(JSON.parse(notes));
             } catch (err) {
-                parsedNotes = [];
+                dataNotes = [];
             }
-            return parsedNotes;
+            return dataNotes;
         });
     }
 
